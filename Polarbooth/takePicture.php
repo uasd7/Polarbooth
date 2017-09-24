3d5d3f42290891460bfb6d7a49c0dfae2ad5968d
Stefan Schreiber <>
2017-09-24 22:44:33 +0200

    screen start

--- Polarbooth/takePicture.php
+++ Polarbooth/takePicture.php
@@ -6,32 +6,31 @@
  * Time: 22:10
  */
 
-require_once('init.php');
+require_once('../config/init.php');
 
 
 $file = md5(time()).'.jpg';
 
-$filenamePhoto = $config['folders']['full'] . DIRECTORY_SEPARATOR . $file;
-$filenameThumb = $config['folders']['thumb'] . DIRECTORY_SEPARATOR . $file;
+$fileNamePhoto = $config['folders']['full'] . DIRECTORY_SEPARATOR . $file;
+$fileNameThumb = $config['folders']['thumb'] . DIRECTORY_SEPARATOR . $file;
 
-$shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename=' . $filenamePhoto . ' images');
+$shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename=' . $fileNamePhoto . ' images');
 
 // image scale
-list($width, $height) = getimagesize($filenamePhoto);
+list($width, $height) = getimagesize($fileNamePhoto);
 $newWidth = 1200;
 $newHeight = $newWidth / $width * $height;
-$source = imagecreatefromjpeg($filenamePhoto);
+$source = imagecreatefromjpeg($fileNamePhoto);
 $thumb = imagecreatetruecolor($newWidth, $newHeight);
 imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-imagejpeg($thumb, $filenameThumb);
+imagejpeg($thumb, $fileNameThumb);
 
 // insert into database
-
 $list = [];
 if (is_file($config['listFile'])) {
     $list = json_decode(file_get_contents($config['listFile']));
 }
-
 $list[] = $file;
 
 file_put_contents($config['listFile'], json_encode($list));
+
