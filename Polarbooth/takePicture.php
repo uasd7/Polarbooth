6efed31dce09b5cf526428526c9e6f4d26d34cea
Stefan Schreiber <>
2017-09-26 20:24:12 +0200

    logging

--- Polarbooth/takePicture.php
+++ Polarbooth/takePicture.php
@@ -9,28 +9,36 @@
 require_once('../config/init.php');
 
 
-$file = md5(time()).'.jpg';
+try {
+    $file = md5(time()).'.jpg';
+    echo 'Filename:'.$file;
 
-$fileNamePhoto = $config['folders']['full'] . DIRECTORY_SEPARATOR . $file;
-$fileNameThumb = $config['folders']['thumb'] . DIRECTORY_SEPARATOR . $file;
+    $fileNamePhoto = $config['folders']['full'].DIRECTORY_SEPARATOR.$file;
+    $fileNameThumb = $config['folders']['thumb'].DIRECTORY_SEPARATOR.$file;
 
-$shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename=' . $fileNamePhoto . ' images');
+    $shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename='.$fileNamePhoto.' images');
+    print_r('Foto');
 
 // image scale
-list($width, $height) = getimagesize($fileNamePhoto);
-$newWidth = 1200;
-$newHeight = $newWidth / $width * $height;
-$source = imagecreatefromjpeg($fileNamePhoto);
-$thumb = imagecreatetruecolor($newWidth, $newHeight);
-imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-imagejpeg($thumb, $fileNameThumb);
+    list($width, $height) = getimagesize($fileNamePhoto);
+    print_r('width: '.$width);
+    print_r('height: '.$height);
+    $newWidth = 1200;
+    $newHeight = $newWidth / $width * $height;
+    $source = imagecreatefromjpeg($fileNamePhoto);
+    $thumb = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+    imagejpeg($thumb, $fileNameThumb);
 
 // insert into database
-$list = [];
-if (is_file($config['listFile'])) {
-    $list = json_decode(file_get_contents($config['listFile']));
-}
-$list[] = $file;
+    $list = [];
+    if (is_file($config['listFile'])) {
+        $list = json_decode(file_get_contents($config['listFile']));
+    }
+    $list[] = $file;
 
-file_put_contents($config['listFile'], json_encode($list));
+    file_put_contents($config['listFile'], json_encode($list));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
