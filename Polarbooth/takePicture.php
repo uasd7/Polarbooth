8168a715accd5862280d3dddb0e1f5bad2b5a7d2
Stefan Schreiber <>
2017-10-13 07:20:24 +0200

    aufgeräumt

--- Polarbooth/takePicture.php
+++ Polarbooth/takePicture.php
@@ -11,24 +11,18 @@ require_once('../config/init.php');
 
 try {
     $file = md5(time()).'.jpg';
-    echo 'Filename:'.$file;
 
     $fileNamePhoto = $config['folders']['full'].DIRECTORY_SEPARATOR.$file;
     $fileNameThumb = $config['folders']['thumb'].DIRECTORY_SEPARATOR.$file;
 
     $shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename='.$fileNamePhoto.' images');
-    echo "\nFoto";
 
 // image scale
     list($width, $height) = getimagesize($fileNamePhoto);
-    echo "\nwidth: ".$width;
-    echo "\nheight: ".$height;
     $newWidth = 1200;
     $newHeight = $newWidth / $width * $height;
     $source = imagecreatefromjpeg($fileNamePhoto);
-    echo "\nsource: ".$source;
     $thumb = imagecreatetruecolor($newWidth, $newHeight);
-    echo "\nthumb: ".$thumb;
     imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
     imagejpeg($thumb, $fileNameThumb);
 
@@ -38,7 +32,6 @@ try {
         $list = json_decode(file_get_contents($config['listFile']));
     }
     $list[] = $config['folders']['thumb'].DIRECTORY_SEPARATOR.$file;
-    echo "\nlist: ".json_encode($list);
 
     file_put_contents($config['listFile'], json_encode($list));
 } catch (Exception $e) {
--- config/init.php
+++ config/init.php
@@ -6,7 +6,7 @@
  * Time: 21:58
  */
 
-require_once('config.php');
+require_once('config.php.dist');
 
 foreach ($config['folders'] as $directory) {
     if (!is_dir($directory)) {
