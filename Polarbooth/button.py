d717847011a0a53ea610da41a5fa39d007cc0a75
Stefan Schreiber <stefan.schreiber@live.de>
2018-08-16 19:23:11 +0200

    Button

---
+++ Polarbooth/button.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(40, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+try:
+  while True:
+    if GPIO.input(40):
+      subprocess.call("/usr/bin/php /var/www/html/Polarbooth/takePicture.php", shell=True)
+    else:
+      time.sleep(0.3)
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
