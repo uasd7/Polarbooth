9b7bc94e084eb930eead2140b2898f768d247b27
Stefan Schreiber <stefan.schreiber@live.de>
2018-08-28 14:51:02 +0200

    Umbau auf Filewatcher

--- Polarbooth/button.py
+++ Polarbooth/button.py
@@ -14,7 +14,7 @@ try:
     if GPIO.input(40):
       subprocess.call("/usr/bin/php /var/www/html/Polarbooth/takePicture.php", shell=True)
     else:
-      time.sleep(0.2)
+      time.sleep(0.1)
 
 except KeyboardInterrupt:
   GPIO.cleanup()
--- Polarbooth/takePicture.php
+++ Polarbooth/takePicture.php
@@ -13,10 +13,7 @@ try {
 
     $fileNamePhoto = $config['folders']['full'].DIRECTORY_SEPARATOR.$file;
     $fileNameThumb = $config['folders']['thumb'].DIRECTORY_SEPARATOR.$file;
-$start = microtime(true);
     $shootimage = shell_exec('sudo gphoto2 --capture-image-and-download --filename='.$fileNamePhoto.' images');
-echo 'imageTakeTime: ' . (microtime(true) - $start);
-$start = microtime(true);
 
 // image scale
     list($width, $height) = getimagesize($fileNamePhoto);
@@ -26,16 +23,6 @@ $start = microtime(true);
     $thumb = imagecreatetruecolor($newWidth, $newHeight);
     imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
     imagejpeg($thumb, $fileNameThumb);
-echo 'imageThumbTime: ' . (microtime(true) - $start);
-
-// insert into database
-    $list = [];
-    if (is_file($config['listFile'])) {
-        $list = json_decode(file_get_contents($config['listFile']));
-    }
-    $list[] = $config['folders']['thumb'].DIRECTORY_SEPARATOR.$file;
-
-    file_put_contents($config['listFile'], json_encode($list));
 } catch (Exception $e) {
     echo $e->getMessage();
 }
